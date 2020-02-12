a4591ade22a2f9f25b83f4f76aa80c723ef199a9
Yoni <yoniakabecky@gmail.com>
2020-02-12 11:08:57 +1100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
 
 export class Navbar extends Component {
   render() {
     return (
-      <div>
-        hello from navbar
-      </div>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus" />
+            my cart
+          </button>
+        </Link>
+      </nav>
     );
   }
 }
