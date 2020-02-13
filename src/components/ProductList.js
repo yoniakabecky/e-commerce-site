4b4bdba0ce77f0dac027837488d7acfb20d7ae7e
Yoni <yoniakabecky@gmail.com>
2020-02-13 10:05:09 -0800

    Setup Context

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
+import { ProductConsumer } from '../context';
 
 export class ProductList extends Component {
-  state = {
-    products: []
-  };
-
   render() {
     return (
       <>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row"></div>
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product product={product} key={product.id} />
+                  })
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </>
-      // <Product />
     );
   }
 }
