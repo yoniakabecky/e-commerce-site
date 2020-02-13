0ad7b81726c7e1dd172a0fc64cad0ad18326d90f
Yoni <yoniakabecky@gmail.com>
2020-02-12 22:03:43 -0800

    Title

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
 
 export class ProductList extends Component {
+  state = {
+    products: []
+  };
+
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row"></div>
+          </div>
+        </div>
+      </>
+      // <Product />
     );
   }
 }
